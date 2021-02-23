00e6c289c6a7a545f5402f06175abe98783e62af
reingxd <60816492+reingxd@users.noreply.github.com>
2021-02-23 18:47:37 +0600

    all working

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
 	<?php require_once('config.php') ?>
 	<?php require_once('./includes/public_functions.php') ?>
+	<?php require_once( ROOT_PATH . '/includes/registration_login.php') ?>
 	<?php $posts = getPublishedPosts(); ?>
 	<?php require_once('./includes/header.php') ?>
 	<?php require_once('./includes/header.php') ?>
@@ -18,9 +19,17 @@
 			<h2 class="content-title">Недавние записи
 			</h2>
 			<hr>
-<?php foreach ($posts as $post): ?>
+			<?php foreach ($posts as $post): ?>
 	<div class="post" style="margin-left: 0px;">
 		<img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
+		<?php if (isset($post['topic']['name'])): ?>
+			<a 
+				href="<?php echo BASE_URL . 'filtered_posts.php?topic=' . $post['topic']['id'] ?>"
+				class="btn category">
+				<?php echo $post['topic']['name'] ?>
+			</a>
+		<?php endif ?>
+
 		<a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
 			<div class="post_info">
 				<h3><?php echo $post['title'] ?></h3>
