ab24d7be76d7ec0bf00a3939ffb09cd25b492468
Alex Sehl <widesehl@gmail.com>
2021-11-12 20:03:55 +0600

    fix: filtered posts styles

--- filtered_posts.php
+++ filtered_posts.php
@@ -13,25 +13,21 @@ if (isset($_GET['topic'])) {
 <body>
 	<?php include(ROOT_PATH . '/includes/navbar.php'); ?>
 	<div class="container">
-		<div class="content">
-			<h2 class="content-title">
-				Посты по теме <u><?php echo getTopicNameById($topic_id); ?></u>
-			</h2>
-			<hr>
+		<h2 class="text-2xl font-normal mb-2">
+			Посты по теме <u><?php echo getTopicNameById($topic_id); ?></u>
+		</h2>
+		<hr class="my-2">
+		<div class="flex flex-wrap gap-2 items-start">
 			<?php foreach ($posts as $post) : ?>
-				<div class="post" style="margin-left: 0px;">
-					<img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
-					<a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
-						<div class="post_info">
-							<h3><?php echo $post['title'] ?></h3>
-							<div class="info">
-								<span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
-								<span class="read_more">Читать больше...</span>
-							</div>
-						</div>
+				<div class="flex flex-col flex-none break-inside bg-white overflow-hidden border-gray-200 shadow-sm p-3 hover:shadow-md duration-75">
+					<img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="max-h-64 max-w-64 h-64 w-64 rounded mb-1" alt="">
+					<a class="hover:underline text-lg font-medium" href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
+						<?php echo $post['title'] ?>
 					</a>
+					<span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
 				</div>
 			<?php endforeach ?>
 		</div>
+	</div>
 	</div>
 	<?php include(ROOT_PATH . '/includes/footer.php'); ?>
